b04e69a41694c0b1496cc6a9f3b113e6b3f79cc0
4epToB <73242484+4epToB@users.noreply.github.com>
2021-02-20 17:51:47 +0300

    main

---
+++ 8_dz/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(8080);
+
+app.use(express.static('public'));
+
+let sockets={}
+let turn
+let unit
+let unitClass
+io.on('connection', function (socket) {
+    if (Object.getOwnPropertyNames(sockets).length==0){
+        turn=Math.round(Math.random())
+        unit=Math.round(Math.random())
+        unitClass=Math.round(Math.random())
+        sockets[socket.id]=socket
+        socket.emit('setNewPlayer',{unit:unit,turn:turn,unitClass:unitClass})
+    } else {
+        sockets[socket.id]=socket
+        socket.emit('setNewPlayer',{unit:!unit,turn:!turn,unitClass:!unitClass})
+    }
+    socket.on('PlayerTurn',function(data){
+        for(var id in sockets){
+			if(id !== socket.id){
+				sockets[id].emit('PlayerTurn', data);
+			}
+		}
+        console.log(data)
+    })
+    socket.on('disconnect', () => {
+		delete sockets[socket.id];
+		console.log(socket.id+"вышел")
+        console.log(Object.getOwnPropertyNames(sockets))
+		}
+        
+	)
+    console.log(Object.getOwnPropertyNames(sockets))
+});
