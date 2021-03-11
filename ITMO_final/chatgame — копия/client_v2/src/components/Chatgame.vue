8b3a8fa0a5d0553662f5040089291d59e9a602bf
4epToB <73242484+4epToB@users.noreply.github.com>
2021-03-12 01:26:35 +0300

    main

---
+++ ITMO_final/chatgame — копия/client_v2/src/components/Chatgame.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="maincontainer">
+      <game></game>
+      <chat></chat>
+  </div>
+</template>
+
+<script>
+import Game from './Game.vue'
+import Chat from './Chat.vue'
+export default {
+  data: function () {
+    return {
+    }
+  },
+  components: {
+    'game':Game,
+    'chat':Chat
+  },
+}
+
+</script>
+
+<style>
+.maincontainer{
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+</style>
